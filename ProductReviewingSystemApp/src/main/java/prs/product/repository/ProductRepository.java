4b7014c29d1512072c81f88ecf62901ab47be9e3
mahmoudjammal <122680563+mahmoudjammal@users.noreply.github.com>
2023-03-04 10:23:37 +0100

    Delete ProductReviewingSystemApp directory

--- ProductReviewingSystemApp/src/main/java/prs/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package prs.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import prs.product.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
